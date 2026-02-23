7ad6a5696517abb0fc0a0829339ec9e0c0a9920e
Ajmal Ali <entizstdio@gmail.com>
2026-02-23 22:04:03 +0530

    asdf

--- letsgo/common/motors.py
+++ letsgo/common/motors.py
@@ -1,43 +1,70 @@
-import RPi.GPIO as GPIO
+from gpiozero import DigitalOutputDevice, PWMOutputDevice
+
 
 class MotorControl:
+    """
+    L298N dual-motor driver using gpiozero (Pi 5 compatible).
+
+    Pin list: [IN1, IN2, IN3, IN4, EN_A, EN_B]
+      • IN1/IN2 control Motor A direction
+      • IN3/IN4 control Motor B direction
+      • EN_A / EN_B are PWM speed pins
+    """
+
     def __init__(self, pins):
-        self.pins = pins # [IN1, IN2, IN3, IN4, EN_A, EN_B]
-        GPIO.setmode(GPIO.BCM)
-        for pin in self.pins:
-            GPIO.setup(pin, GPIO.OUT)
-        self.pwm_a = GPIO.PWM(pins[4], 1000)
-        self.pwm_b = GPIO.PWM(pins[5], 1000)
-        self.pwm_a.start(0)
-        self.pwm_b.start(0)
+        self.pins = pins  # [IN1, IN2, IN3, IN4, EN_A, EN_B]
+
+        # Direction pins (digital HIGH / LOW)
+        self.in1 = DigitalOutputDevice(pins[0])
+        self.in2 = DigitalOutputDevice(pins[1])
+        self.in3 = DigitalOutputDevice(pins[2])
+        self.in4 = DigitalOutputDevice(pins[3])
+
+        # Speed pins (PWM, 1 kHz)
+        self.pwm_a = PWMOutputDevice(pins[4], frequency=1000)
+        self.pwm_b = PWMOutputDevice(pins[5], frequency=1000)
+
+    def _set_direction(self, in1, in2, in3, in4):
+        """Set the four direction pins (True/False)."""
+        self.in1.value = in1
+        self.in2.value = in2
+        self.in3.value = in3
+        self.in4.value = in4
 
     def move(self, direction, speed=50):
+        """
+        Move the chassis in the given direction.
+
+        Args:
+            direction: 'forward', 'backward', 'left', or 'right'.
+            speed:     0-100 (percentage of full power).
+        """
         if direction == "forward":
-            GPIO.output(self.pins[0], True); GPIO.output(self.pins[1], False)
-            GPIO.output(self.pins[2], True); GPIO.output(self.pins[3], False)
+            self._set_direction(True, False, True, False)
         elif direction == "left":
-            GPIO.output(self.pins[0], False); GPIO.output(self.pins[1], True)
-            GPIO.output(self.pins[2], True); GPIO.output(self.pins[3], False)
+            self._set_direction(False, True, True, False)
         elif direction == "right":
-            GPIO.output(self.pins[0], True); GPIO.output(self.pins[1], False)
-            GPIO.output(self.pins[2], False); GPIO.output(self.pins[3], True)
+            self._set_direction(True, False, False, True)
         elif direction == "backward":
-            GPIO.output(self.pins[0], False); GPIO.output(self.pins[1], True)
-            GPIO.output(self.pins[2], False); GPIO.output(self.pins[3], True)
-        
-        self.pwm_a.ChangeDutyCycle(speed)
-        self.pwm_b.ChangeDutyCycle(speed)
+            self._set_direction(False, True, False, True)
+
+        # gpiozero PWM uses 0.0–1.0 range
+        duty = max(0.0, min(speed / 100.0, 1.0))
+        self.pwm_a.value = duty
+        self.pwm_b.value = duty
 
     def stop(self):
-        GPIO.output(self.pins[0], False); GPIO.output(self.pins[1], False)
-        GPIO.output(self.pins[2], False); GPIO.output(self.pins[3], False)
-        self.pwm_a.ChangeDutyCycle(0)
-        self.pwm_b.ChangeDutyCycle(0)
-        
+        """Stop both motors immediately."""
+        self._set_direction(False, False, False, False)
+        self.pwm_a.value = 0
+        self.pwm_b.value = 0
+
     def cleanup(self):
-        if GPIO.getmode() is None:
-            return
+        """Release all GPIO resources."""
         self.stop()
-        self.pwm_a.stop()
-        self.pwm_b.stop()
-        GPIO.cleanup()
+        self.in1.close()
+        self.in2.close()
+        self.in3.close()
+        self.in4.close()
+        self.pwm_a.close()
+        self.pwm_b.close()
