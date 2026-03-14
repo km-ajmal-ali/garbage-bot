e607e91ef687b864eefb47c9395f7f7608660ed0
Ajmal Ali <entizstdio@gmail.com>
2026-03-14 23:10:58 +0530

    test for motor

--- letsgo/test/test_motors.py
+++ letsgo/test/test_motors.py
@@ -6,7 +6,7 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
-from letsgo.common.motors import MotorControl
+from common.motors import MotorControl
 
 def main():
     print("=== Motor Testing Tool ===")
